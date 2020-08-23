7b67d6ea04e3b0f714b4b2692c4439a6a962f28b
James <james.ang@acceval-intl.com>
2020-08-23 23:14:22 +0800

    Complete demand centre 1

--- DRP/drp.py
+++ DRP/drp.py
@@ -42,7 +42,12 @@ dc1['Planned Receipts (MT)'] = 0
 dc1['Planned Orders (MT)'] = 0
 dc1['Num of 10T Trucks'] = 0
 dc1['Num of 20T Trucks'] = 0
-dc1['Total Shipment Cost (RM)'] = 0
+dc1['Total Transportation Cost (RM)'] = 0
+dc1['Product Cost (RM)'] = 0
+dc1['Total Supply Cost (RM)'] = 0
+dc1['Revenue'] = 0
+dc1['Profit'] = 0
+
 first_week = dc1.index[0]
 dc1.loc[first_week,'Proj End Inv'] = dc1_params.loc['Value','Initial Ending Inventory (MT)'] +\
                                     dc1.loc[first_week,'Scheduled Receipts (MT)'] +\
@@ -57,6 +62,14 @@ TD = dc1.columns.get_loc('Total Demand')
 PO = dc1.columns.get_loc('Planned Orders (MT)')
 n10T = dc1.columns.get_loc('Num of 10T Trucks')
 n20T = dc1.columns.get_loc('Num of 20T Trucks')
+TTC = dc1.columns.get_loc('Total Transportation Cost (RM)')
+PUC = dc1.columns.get_loc('Product Unit Cost (RM/MT)')
+PC = dc1.columns.get_loc('Product Cost (RM)')
+TSC = dc1.columns.get_loc('Total Supply Cost (RM)')
+REV = dc1.columns.get_loc('Revenue')
+PF = dc1.columns.get_loc('Profit')
+PSP = dc1.columns.get_loc('Product Selling Price (RM/MT)')
+
 SS = dc1_params.columns.get_loc('Safety Stock (MT)')
 LT = dc1_params.columns.get_loc('Lead Time (weeks)')
 LS = dc1_params.columns.get_loc('Lot Size (MT)')
@@ -76,11 +89,22 @@ for i in range(1, len(dc1)):
     dc1.iloc[i, PEI] = dc1.iloc[i-1, PEI] + dc1.iloc[i, SR] + dc1.iloc[i, PR] - dc1.iloc[i, TD]
 
 for i in range(0, len(dc1)-int(dc1_params.iloc[0, LT])):
-    # print(i)
     dc1.iloc[i, PO] = dc1.iloc[i + int(dc1_params.iloc[0, LT]), PR]
+    
     # Adding truck count
     dc1.iloc[i, n10T] = ((dc1.iloc[i, PO] + dc1_params.iloc[0, SC10T] - 1)%dc1_params.iloc[0, SC20T])//dc1_params.iloc[0, SC10T]
     dc1.iloc[i, n20T] = (dc1.iloc[i, PO] + dc1_params.iloc[0, SC10T] - 1)//dc1_params.iloc[0, SC20T]
     
+    dc1.iloc[i, TTC] = dc1.iloc[i, n10T]*dc1_params.iloc[1, SC10T] + dc1.iloc[i, n20T]*dc1_params.iloc[1, SC20T]
+    dc1.iloc[i, PC] = dc1.iloc[i, PUC]*dc1.iloc[i, PO]
+    dc1.iloc[i, TSC] = dc1.iloc[i, TTC] + dc1.iloc[i, PC]
+    dc1.iloc[i, REV] = dc1.iloc[i, PSP]*dc1.iloc[i, PO]
+    dc1.iloc[i, PF] = dc1.iloc[i, REV] - dc1.iloc[i, TSC]
+    
+#%% Demand Centre 2
 
-
+#% Import input data
+# dc1 = pd.read_excel('demand_data.xlsx',sheet_name='DC1', skiprows = 8, usecols = 'B:L')
+# dc1.index = dc1['Category']
+# dc1.drop('Category', axis=1, inplace=True)
+# dc1 = dc1.transpose().fillna(0)
