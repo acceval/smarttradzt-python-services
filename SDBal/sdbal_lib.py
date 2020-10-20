36e1bb79f4d8c5e6de42fc4884dafbe682366ec4
James <james.ang@acceval-intl.com>
2020-10-20 09:25:42 +0800

    Changed Safety Stock to Target Ending Inventory

--- SDBal/sdbal_lib.py
+++ SDBal/sdbal_lib.py
@@ -36,52 +36,63 @@ class Supp_Params:
 
 class Gen_Supp_Plan:
     
-    def __init__(self, excelfile, sheet_name, threemonths_demand_forecast, prod_qty_m, initial_end_inv, final_month_end_total_order):
+    def __init__(self, excelfile, sheet_name, threemonths_demand_forecast, prod_qty_m, initial_end_inv, final_month_end_total_order, max_prod_limit, target_ending_inventory):
         self.n = 3
         self.excelfile = excelfile
         self.sheet_name = sheet_name
+        self.max_prod_limit = max_prod_limit
+        self.target_ending_inventory = target_ending_inventory
         
         # Supplier parameters
         self.table = pd.read_excel(self.excelfile, sheet_name=self.sheet_name, skiprows = 6, nrows = 5, usecols = 'C:D')
         self.table.index = self.table['Params']
-        self.table.drop('Params', axis=1, inplace=True)
+        self.table.drop('Params', axis = 1, inplace = True)
         
-        # self.initial_end_inv = self.table.loc['Initial Inventory','Value']
         self.initial_end_inv = initial_end_inv
         self.safety_stock = self.table.loc['Safety Stock','Value']
         self.final_month_end_total_order = final_month_end_total_order
         self.demand_forecast_month_m_minus_1 = threemonths_demand_forecast
                 
         # Initialisation
-        # self.starting_inventory = [0]*self.n
         self.prod_plan = [0]*self.n
-        self.planned_starting_inventory = [0]*self.n
-        self.planned_ending_inventory = [0]*self.n
-        self.planned_available_inventory = [0]*self.n
-        self.planned_available_supply_qty = [0]*self.n
-        self.planned_supply_demand_balance = [0]*self.n
-        # self.updated_prod_plan = [0]*self.n
+        self.starting_inventory = [0]*self.n
+        self.ending_inventory = [0]*self.n
+        self.available_inventory = [0]*self.n
+        self.available_supply_qty = [0]*self.n
+        self.supply_demand_balance = [0]*self.n
+        self.spot_allocation_to_reduce = [0]*self.n
         
         self.prod_plan[0] = prod_qty_m
-        self.planned_starting_inventory[0] = self.initial_end_inv + self.prod_plan[0]
-        self.planned_available_inventory[0] = self.initial_end_inv - self.safety_stock
-        self.planned_ending_inventory[0] = self.planned_starting_inventory[0] - self.demand_forecast_month_m_minus_1[0]
-        self.planned_available_supply_qty[0] = self.planned_available_inventory[0] + self.prod_plan[0]
-        self.planned_supply_demand_balance[0] = self.planned_available_supply_qty[0] - self.demand_forecast_month_m_minus_1[0]
-        # self.initial_end_inv_month_m = self.planned_ending_inventory[0]
+        self.starting_inventory[0] = self.initial_end_inv + self.prod_plan[0]
+        self.available_inventory[0] = self.initial_end_inv - self.target_ending_inventory[0]
+        self.ending_inventory[0] = self.starting_inventory[0] - self.demand_forecast_month_m_minus_1[0]
+        self.available_supply_qty[0] = self.available_inventory[0] + self.prod_plan[0]
+        self.supply_demand_balance[0] = self.available_supply_qty[0] - self.demand_forecast_month_m_minus_1[0]
+        # self.initial_end_inv_month_m = self.ending_inventory[0]
         
         # Updates months m-1
         for i in range(1,self.n):
             # print(i)           
-            self.planned_available_inventory[i] = self.planned_ending_inventory[i-1] - self.safety_stock
-            self.prod_plan[i] = self.demand_forecast_month_m_minus_1[i] - self.planned_available_inventory[i]
-            self.planned_starting_inventory[i] = self.planned_ending_inventory[i-1] + self.prod_plan[i]
-            self.planned_ending_inventory[i] = self.planned_starting_inventory[i] - self.demand_forecast_month_m_minus_1[i]      
-            self.planned_available_supply_qty[i] = self.planned_available_inventory[i] + self.prod_plan[i]
-            self.planned_supply_demand_balance[i] = self.planned_available_supply_qty[i] - self.demand_forecast_month_m_minus_1[i]
+            self.available_inventory[i] = self.ending_inventory[i-1] - self.target_ending_inventory[i]
+            self.prod_plan[i] = self.demand_forecast_month_m_minus_1[i] - self.available_inventory[i]
+            
+            if self.prod_plan[i] > self.max_prod_limit:
+                self.spot_allocation_to_reduce[i] = self.prod_plan[i] - self.max_prod_limit
+                self.prod_plan[i] = self.max_prod_limit
+                print(f"Production Quantity in Month {i+1} is above production limit. Reduce production quantity by {self.spot_allocation_to_reduce[i]} MT.")
+            else:
+                pass
+            
+            self.starting_inventory[i] = self.ending_inventory[i-1] + self.prod_plan[i]
+            self.ending_inventory[i] = self.starting_inventory[i] - self.demand_forecast_month_m_minus_1[i]      
+            self.available_supply_qty[i] = self.available_inventory[i] + self.prod_plan[i]
+            self.supply_demand_balance[i] = self.available_supply_qty[i] - self.demand_forecast_month_m_minus_1[i]
             # self.updated_prod_plan[i-1] = self.prod_plan[i]
         
         # Outputs
-        self.initial_end_inv_next_month = self.planned_starting_inventory[0] - self.final_month_end_total_order
-        self.supply_plan_m = self.prod_plan[1]
+        self.initial_end_inv_next_month = self.starting_inventory[0] - self.final_month_end_total_order
+        if self.initial_end_inv_next_month < self.safety_stock:
+            print('Ending Inventory is below Safety Stock level at the end of execution month.')
+        
+        self.supply_plan_next_month = self.prod_plan[1]
         
--- SDBal/sdbal_main.py
+++ SDBal/sdbal_main.py
@@ -10,6 +10,7 @@ import os
 os.chdir(r'C:/Users/User/Documents/ghub_acceval/smarttradzt-python-services/SDBal/')
 
 #%% Import libraries
+import pandas as pd
 from sdbal_lib import Month_Demand, Month_Order, Gen_Supp_Plan
 
 #%% Import Data
@@ -30,31 +31,46 @@ monthorder_m3 = Month_Order(excelfilename, 'rank_dec')
 threemonths_demand_m_minus_1 = [monthdemand_m, monthdemand_m1, monthdemand_m2]
 threemonths_demand_forecast = [month.totaldemand for month in threemonths_demand_m_minus_1]
 
-# Supply plan month m-1
-prod_qty_m = 2350  # For month m (Sept)
+# prod_qty_m = 2350     # Supply plan month m (Sept)
+prod_qty_m = pd.read_excel(excelfilename, sheet_name='S&DBalancing', skiprows = 24, nrows = 1, usecols = 'L').iloc[0,0]
 prod_qty = prod_qty_m
 
-ini_end_inv_m_minus_1 = 450
+# maximum_production_limit = 2400
+maximum_production_limit = pd.read_excel(excelfilename, sheet_name='S&DBalancing', skiprows = 8, nrows = 1, usecols = 'D').iloc[0,0]
+
+# ini_end_inv_m_minus_1 = 450
+ini_end_inv_m_minus_1 = pd.read_excel(excelfilename, sheet_name='S&DBalancing', skiprows = 26, nrows = 1, usecols = 'K').iloc[0,0]
 ini_end_inv = ini_end_inv_m_minus_1
 
-final_month_end_total_order_m = 2300
+# target_ending_inventory = [250, 300, 260]
+target_ending_inventory = pd.read_excel(excelfilename, sheet_name='S&DBalancing', skiprows = 27, nrows = 1, usecols = 'L:N').iloc[0,:].to_list()
+
+# final_month_end_total_order_m = 2650  # Manual Entry
+final_month_end_total_order_m = pd.read_excel(excelfilename, sheet_name='S&DBalancing', skiprows = 23, nrows = 1, usecols = 'L').iloc[0,0]
 final_month_end_total_order = final_month_end_total_order_m
 
 # Supply plan month m-1
-supp_plan_m_minus_1 = Gen_Supp_Plan(excelfilename, 'S&DBalancing', threemonths_demand_forecast, prod_qty, ini_end_inv, final_month_end_total_order)
+print('\nCalculating month m-1...')
+supp_plan_m_minus_1 = Gen_Supp_Plan(excelfilename, 'S&DBalancing', threemonths_demand_forecast, prod_qty, ini_end_inv, final_month_end_total_order, maximum_production_limit, target_ending_inventory)
 
 #%% simulate month
 
 # Demand plan month m
 threemonths_demand_m = [monthdemand_m1, monthdemand_m2, monthdemand_m3]
-threemonths_demand_forecast = [month.totaldemand for month in threemonths_demand_m]
-# threemonths_demand_forecast = [2365, 2500, 2285]
+# threemonths_demand_forecast = [month.totaldemand for month in threemonths_demand_m]
+threemonths_demand_forecast = [2365, 3200, 2285]
+
 # Supply plan month m-1
-prod_qty = supp_plan_m_minus_1.supply_plan_m
+prod_qty = supp_plan_m_minus_1.supply_plan_next_month
 
 ini_end_inv = supp_plan_m_minus_1.initial_end_inv_next_month
 
-final_month_end_total_order_m = 2000
+# target_ending_inventory = [250, 300, 260]
+target_ending_inventory = pd.read_excel(excelfilename, sheet_name='S&DBalancing2', skiprows = 27, nrows = 1, usecols = 'L:N').iloc[0,:].to_list()
+
+# final_month_end_total_order_m = 2000
+final_month_end_total_order_m = pd.read_excel(excelfilename, sheet_name='S&DBalancing2', skiprows = 23, nrows = 1, usecols = 'L').iloc[0,0]
 final_month_end_total_order = final_month_end_total_order_m
 
-supp_plan_m = Gen_Supp_Plan(excelfilename, 'S&DBalancing', threemonths_demand_forecast, prod_qty, ini_end_inv, final_month_end_total_order)
+print('\nCalculating month m...')
+supp_plan_m = Gen_Supp_Plan(excelfilename, 'S&DBalancing', threemonths_demand_forecast, prod_qty, ini_end_inv, final_month_end_total_order, maximum_production_limit, target_ending_inventory)
