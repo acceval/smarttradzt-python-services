92ae10050fb4acfa3a07caf7382c14a4d0fbe7e0
James <james.ang@acceval-intl.com>
2020-11-02 13:28:07 +0800

    Alter alloc to trim for country

--- SDBal/exec.py
+++ SDBal/exec.py
@@ -44,7 +44,6 @@ if cut_method == 'Country level':
     country_total = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['country_rank'])['Forecast'].sum()))
     country_total['Percent cut'] = 0
     country_total['Max cut qty'] = 0
-        
     
     for i in range(0,len(country_total)):
         if 'A' in country_total.index[i]:
@@ -69,32 +68,64 @@ if cut_method == 'Country level':
             
         else:
             pass
-    
        
     total_max_cut_qty = country_total['Max cut qty'].sum()
+    total_max_cut_qty_ori = total_max_cut_qty
     country_total['Country'] = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['country_rank'])['ship_to_country'].unique()))
     country_total['Country'] = [a[0] for a in country_total['Country']]
     
     
-    if alloc_to_trim <= total_max_cut_qty:
-        pass
-    else:    
+    while alloc_to_trim > total_max_cut_qty:
+    #     pass
+    # else:    
         print(f'Allocation Quantity to Trim ({alloc_to_trim} MT) calculated from Supply Demand Plan \
     \nis more than the Total Allowable Trim Quantity ({total_max_cut_qty} MT) set according to Country Trim Percentage.\
-    \nTherefore, Maximum Allocation Trim Quantity is set at {total_max_cut_qty} MT.')
-        alloc_to_trim = total_max_cut_qty
+    \nTherefore, additional {alloc_to_trim - total_max_cut_qty} MT will be trimmed above user setting.')
+        # alloc_to_trim = total_max_cut_qty
+        country_perctg_cut.loc[:,'perctg cut'] = country_perctg_cut.loc[:,'perctg cut'] + country_perctg_cut.loc[:,'perctg cut']*0.1
+        country_perctg_cut.loc[country_perctg_cut.loc[:,'perctg cut']>100,'perctg cut'] = 100
         
+        country_total = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['country_rank'])['Forecast'].sum()))
+        country_total['Percent cut'] = 0
+        country_total['Max cut qty'] = 0
+        
+        for i in range(0,len(country_total)):
+            if 'A' in country_total.index[i]:
+                country_total['Max cut qty'][i] = country_total['Forecast'][i] * 0.01 * country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'A', 'perctg cut']
+                country_total['Percent cut'][i] = country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'A', 'perctg cut']
+                
+            elif 'B' in country_total.index[i]:
+                country_total['Max cut qty'][i] = country_total['Forecast'][i] * 0.01 * country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'B', 'perctg cut']
+                country_total['Percent cut'][i] = country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'B', 'perctg cut']
+                
+            elif 'C' in country_total.index[i]:
+                country_total['Max cut qty'][i] = country_total['Forecast'][i] * 0.01 * country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'C', 'perctg cut']
+                country_total['Percent cut'][i] = country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'C', 'perctg cut']
+                
+            elif 'D' in country_total.index[i]:
+                country_total['Max cut qty'][i] = country_total['Forecast'][i] * 0.01 * country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'D', 'perctg cut']
+                country_total['Percent cut'][i] = country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'D', 'perctg cut']
+            
+            elif 'P' in country_total.index[i]:
+                country_total['Max cut qty'][i] = country_total['Forecast'][i] * 0.01 * country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'P', 'perctg cut']
+                country_total['Percent cut'][i] = country_perctg_cut.loc[country_perctg_cut['country_rank'] == 'P', 'perctg cut']
+                
+            else:
+                pass
+       
+        total_max_cut_qty = country_total['Max cut qty'].sum()
+        country_total['Country'] = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['country_rank'])['ship_to_country'].unique()))
+        country_total['Country'] = [a[0] for a in country_total['Country']]
+    
     accumulated_all_country_trimmed_quantity = 0
-        
-        
     
     # Looping Through Country
     for i in range(len(country_total),0,-1):    # <-- Count backwards because lowest rank to be trimmed first
         
-        if accumulated_all_country_trimmed_quantity >= alloc_to_trim:
-            print('ALLOCATION PLAN (Country Level) COMPLETED!')
+        # if accumulated_all_country_trimmed_quantity >= alloc_to_trim:
+        #     print('ALLOCATION PLAN (Country Level) COMPLETED!')
             
-            break
+        #     break
         
         accumulated_trimmed_qty = 0
         qty_to_trim = country_total['Max cut qty'][i-1]
@@ -114,10 +145,10 @@ if cut_method == 'Country level':
         # Looping Through Individual Customers/Companies
         for j in range(0,len(temp_df)):
             
-            if accumulated_all_country_trimmed_quantity >= alloc_to_trim:
-                print(f"\nEnd of Allocation adjustment for {temp_df['ship_to_country'].unique().tolist()[0]}.")
-                print("===================================================================================")
-                break
+            # if accumulated_all_country_trimmed_quantity >= alloc_to_trim:
+            #     print(f"\nEnd of Allocation adjustment for {temp_df['ship_to_country'].unique().tolist()[0]}.")
+            #     print("===================================================================================")
+            #     break
             
             print('\n\tCustomer: ' + str(temp_df['cust_code'].iloc[j]))
             print('\tCustomer rank: ' + str(temp_df['customer_rank'].iloc[j]))
@@ -151,25 +182,29 @@ if cut_method == 'Country level':
                 
                 if accumulated_all_country_trimmed_quantity + last_cut >  alloc_to_trim:
                     last_cut = alloc_to_trim - accumulated_all_country_trimmed_quantity
-                
-                
+                                
                 print(f"\t\tReduce Allocation Quantity by {last_cut} MT from customer '{temp_df['cust_code'].iloc[j]}'.")
                 accumulated_trimmed_qty+= last_cut
                 accumulated_all_country_trimmed_quantity += last_cut
                 print(f"\t\tAccumulated Trimmed Quantity for {temp_df['ship_to_country'].unique().tolist()[0]} = {accumulated_trimmed_qty} MT.")
                 print(f"\t\tAccumulated Trimmed Quantity for All Countries = {accumulated_all_country_trimmed_quantity} MT.")
-                # if accumulated_all_country_trimmed_quantity > alloc_to_trim:
-                #     print(f"\t\tQUANTITY IS NOW ABOVE LEVEL! : {accumulated_all_country_trimmed_quantity} MT")
-                #     break
-                # else:
-                    # print(f"\t\tAccumulated Trimmed Quantity for {temp_df['ship_to_country'].unique().tolist()[0]} = {accumulated_trimmed_qty} MT.")
-                    # print(f"\t\tAccumulated Trimmed Quantity for All Countries = {accumulated_all_country_trimmed_quantity} MT.")
-                    
+    
                 rank_by_country.loc[ind_country[j],'Final Allocated Quantity'] = temp_df.loc[ind_country[j],'Forecast'] - last_cut
                 print(f"\t\tFinal Allocated Quantity: {rank_by_country.loc[ind_country[j],'Final Allocated Quantity']} MT")
                 print(f"\nEnd of Allocation planning for {temp_df['ship_to_country'].unique().tolist()[0]}.")
                 print("===================================================================================")
                 break
+            
+            if accumulated_all_country_trimmed_quantity >= alloc_to_trim:
+                print(f"\nEnd of Allocation adjustment for {temp_df['ship_to_country'].unique().tolist()[0]}.")
+                print("===================================================================================")
+                break
+        
+        
+        if accumulated_all_country_trimmed_quantity >= alloc_to_trim:
+            print('ALLOCATION PLAN (Country Level) COMPLETED!')
+            
+            break
 
 #% By Customer
 elif cut_method == 'Customer level':
@@ -184,13 +219,10 @@ elif cut_method == 'Customer level':
     
     # Totals
     customer_total = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['customer_rank'])['Forecast'].sum()))
-    # customer_total = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['customer_rank', 'country_rank'])['Forecast'].sum()))
-
     customer_total['Percent cut'] = 0
     customer_total['Max cut qty'] = 0
     
     for i in range(0,len(customer_total)):
-        # print(i)
         if 'A' in customer_total.index[i]:
             customer_total['Max cut qty'][i] = customer_total['Forecast'][i] * 0.01 * customer_perctg_cut.loc[customer_perctg_cut['customer_rank'] == 'A', 'cust_perctg cut']
             customer_total['Percent cut'][i] = customer_perctg_cut.loc[customer_perctg_cut['customer_rank'] == 'A', 'cust_perctg cut']
@@ -215,13 +247,11 @@ elif cut_method == 'Customer level':
             pass
     
     total_max_cut_qty = customer_total['Max cut qty'].sum()
-
-    # customer_total['Country'] = pd.DataFrame(copy.deepcopy(all_ranks.groupby(['customer_rank','country_rank'])['ship_to_country'].unique()))
-    # customer_total['Country'] = [a[0] for a in customer_total['Country']]
+    total_max_cut_qty_ori = total_max_cut_qty
 
     if alloc_to_trim <= total_max_cut_qty:
         pass
-    else:        
+    else:
         print(f'Allocation Quantity to Trim ({alloc_to_trim} MT) calculated from Supply Demand Plan \
     \nis more than the Total Allowable Trim Quantity ({total_max_cut_qty} MT) set according to Country Trim Percentage.\
     \nTherefore, Maximum Allocation Trim Quantity is set at {total_max_cut_qty} MT.')
@@ -233,11 +263,6 @@ elif cut_method == 'Customer level':
     
     for i in range(len(customer_total),0,-1):    # <-- Count backwards because lowest rank to be trimmed first
         
-        if accumulated_all_customer_trimmed_quantity >= alloc_to_trim:
-            print('ALLOCATION PLAN (Customer Level) COMPLETED!')
-            
-            break
-
         accumulated_trimmed_qty = 0
         qty_to_trim = customer_total['Max cut qty'][i-1]
         temp_df = copy.deepcopy(rank_by_customer.loc[rank_by_customer['customer_rank'] == customer_total.index[i-1], :])
@@ -257,10 +282,10 @@ elif cut_method == 'Customer level':
         # Looping Through Individual Countries
         for j in range(0,len(temp_df)):
             
-            if accumulated_all_customer_trimmed_quantity >= alloc_to_trim:
-                print(f"\nEnd of Allocation adjustment for {str(customer_total.index[i-1])}.")
-                print("===================================================================================")
-                break
+            # if accumulated_all_customer_trimmed_quantity >= alloc_to_trim:
+            #     print(f"\nEnd of Allocation adjustment for {str(customer_total.index[i-1])}.")
+            #     print("===================================================================================")
+            #     break
             
             print('\n\tCountry: ' + str(temp_df['ship_to_country'].iloc[j]))
             print('\tCountry rank: ' + str(temp_df['country_rank'].iloc[j]))
@@ -307,3 +332,13 @@ elif cut_method == 'Customer level':
                 print(f"\nEnd of Allocation planning Customer Rank {str(customer_total.index[i-1])}.")
                 print("===================================================================================")
                 break
+            
+            if accumulated_all_customer_trimmed_quantity >= alloc_to_trim:
+                print(f"\nEnd of Allocation adjustment for {str(customer_total.index[i-1])}.")
+                print("===================================================================================")
+                break
+            
+        if accumulated_all_customer_trimmed_quantity >= alloc_to_trim:
+            print('ALLOCATION PLAN (Customer Level) COMPLETED!')
+            
+            break
