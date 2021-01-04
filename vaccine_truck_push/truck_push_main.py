908a65dbfbfed370346296001b0bc70e05600cd7
James <james.ang@acceval-intl.com>
2021-01-04 22:20:18 +0800

    updated truck push with leadtime api

--- vaccine_truck_push/truck_push_main.py
+++ vaccine_truck_push/truck_push_main.py
@@ -5,14 +5,17 @@ Created on Tue Dec 22 23:55:14 2020
 @author: James Ang
 """
 
-
 import json
+import requests
 from truck_push_lib import Allocations, Warehouses, Trucks, Suppliers, Routes, Entities, retjson
   
 # Opening JSON file 
-f = open('input_truck_push.json',) 
+f = open('input_truck_push.json',)
+
+# g = open('input_leadtime.json',) 
   
 data_json = json.load(f)
+# data_json2 = json.load(g)
 
 data_allocation = data_json.get('bi-weekly_allocation')
 
@@ -20,11 +23,16 @@ data_warehouses = data_json.get('warehouses')
 
 data_suppliers = data_json.get('suppliers')
 
-data_entities = data_json.get('entity_properties')
+# data_entities = data_json2.get('entity_properties')
 
 data_trucks = data_json.get('trucks')
 
 data_routes = data_json.get('routes')
+
+# API
+response = requests.get("https://office.smarttradzt.com:8001/buy-ecommerce-service/lead-time/delivery/findAll/202012120000195")
+
+data_entities = response.json()
 
 #%% 
 allocation_all = [Allocations(data) for data in data_allocation]
